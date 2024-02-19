d7b5e7c13676fe39626235a15df394474184aee0
Vojtech Mares <iam@vojtechmares.com>
2024-02-19 22:37:54 +0100

    feat(infrastructure): bump
    - filter k8s version by minor
    - add helm charts
      - ingress-nginx
      - cert-manager
      - kube-prometheus-stack
    - cert-manager ClusterIssuer letsencrypt-prod and letsencrypt-stage

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,9 +1,17 @@
+locals {
+  kubernetes_minor_version = "1.28"
+}
+
+data "cloudflare_zone" "demo" {
+  name = "maresdemo.com"
+}
+
 data "digitalocean_kubernetes_versions" "doks" {}
 
-resource "digital_ocean_kubernetes_cluster" "demo" {
-  name    = "training-demo"
+resource "digitalocean_kubernetes_cluster" "demo" {
+  name    = "training-demo-k8s"
   region  = "fra1"
-  version = data.digitalocean_kubernetes_versions.doks.latest_version
+  version = [for version in data.digitalocean_kubernetes_versions.doks.versions : version.version if startswith(version, local.kubernetes_minor_version)][0]
 
   node_pool {
     name       = "default"
@@ -12,10 +20,161 @@ resource "digital_ocean_kubernetes_cluster" "demo" {
   }
 }
 
-# TODO: Add Helm release for ingress-nginx
-# TODO: Add Helm release for cert-manager
-# TODO: Add Helm release for external-dns
-# TODO: Add Helm release for prometheus-operator
-# TODO: Add Helm release for kube-prometheus-stack
+output "kubeconfig" {
+  value     = digitalocean_kubernetes_cluster.demo.kube_config[0].raw_config
+  sensitive = true
+}
 
-# TODO: Add cloudflare_api_token and create token on fly for external-dns
+resource "digitalocean_loadbalancer" "demo" {
+  name   = "training-demo-lb"
+  region = "fra1"
+
+  droplet_tag = "k8s:${digitalocean_kubernetes_cluster.demo.id}"
+
+  healthcheck {
+    port     = 32080
+    protocol = "tcp"
+  }
+
+  forwarding_rule {
+    entry_port      = 80
+    target_port     = 32080
+    entry_protocol  = "tcp"
+    target_protocol = "tcp"
+  }
+
+  forwarding_rule {
+    entry_port      = 443
+    target_port     = 32443
+    entry_protocol  = "tcp"
+    target_protocol = "tcp"
+  }
+
+  depends_on = [
+    digitalocean_kubernetes_cluster.demo,
+  ]
+}
+
+resource "cloudflare_record" "wildcard" {
+  zone_id = data.cloudflare_zone.demo.zone_id
+  name    = "*.k8s"
+  value   = digitalocean_loadbalancer.demo.ip
+  type    = "A"
+  ttl     = 300
+  proxied = false
+}
+
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = "4.9.1"
+  create_namespace = true
+
+  atomic  = true
+  timeout = 900
+
+  values = [
+    file("./helm-values/ingress-nginx.yaml")
+  ]
+
+  depends_on = [
+    digitalocean_kubernetes_cluster.demo
+  ]
+}
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  version          = "1.14.2"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  atomic  = true
+  timeout = 900
+
+  values = [
+    file("./helm-values/cert-manager.yaml"),
+  ]
+
+  depends_on = [
+    digitalocean_kubernetes_cluster.demo,
+  ]
+}
+
+resource "time_sleep" "wait_for_crds" {
+  depends_on = [
+    helm_release.cert_manager,
+    helm_release.ingress_nginx,
+  ]
+
+  create_duration = "30s"
+}
+
+resource "kubectl_manifest" "clusterissuer_letsencrypt_prod" {
+  depends_on = [
+    time_sleep.wait_for_crds,
+  ]
+
+  yaml_body = <<YAML
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-prod
+spec:
+  acme:
+    email: nobody@maresdemo.com
+    server: https://acme-v02.api.letsencrypt.org/directory
+    privateKeySecretRef:
+      name: letsencrypt-prod
+    solvers:
+    - http01:
+        ingress:
+          class: nginx
+YAML
+}
+
+resource "kubectl_manifest" "clusterissuer_letsencrypt_staging" {
+  depends_on = [
+    time_sleep.wait_for_crds,
+  ]
+
+  yaml_body = <<YAML
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-staging
+spec:
+  acme:
+    email: nobody@maresdemo.com
+    server: https://acme-staging-v02.api.letsencrypt.org/directory
+    privateKeySecretRef:
+      name: letsencrypt-staging
+    solvers:
+    - http01:
+        ingress:
+          class: nginx
+YAML
+}
+
+resource "helm_release" "kube_promehtheus_stack" {
+  name             = "monitoring"
+  namespace        = "monitoring"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "56.7.0"
+  create_namespace = true
+
+  atomic  = true
+  timeout = 900
+
+  values = [
+    file("./helm-values/kube-prometheus-stack.yaml"),
+  ]
+
+  depends_on = [
+    digitalocean_kubernetes_cluster.demo,
+  ]
+}
--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -2,16 +2,77 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~>2.34.1"
+      version = "2.34.1"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.24.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "2.0.4"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "0.10.0"
     }
   }
 }
 
-variable "do_token" {
-  type      = string
-  sensitive = true
+variable "digitalocean_token" {
+  description = "DigitalOcean API token"
+  type        = string
+  sensitive   = true
 }
 
 provider "digitalocean" {
-  token = var.do_token
+  token = var.digitalocean_token
 }
+
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token"
+  type        = string
+  sensitive   = true
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+provider "helm" {
+  kubernetes {
+    host  = digitalocean_kubernetes_cluster.demo.endpoint
+    token = digitalocean_kubernetes_cluster.demo.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      digitalocean_kubernetes_cluster.demo.kube_config[0].cluster_ca_certificate
+    )
+  }
+}
+
+provider "kubectl" {
+  # Do not load host's ~/.kube/config or $KUBECONFIG file
+  load_config_file = false
+
+  host  = digitalocean_kubernetes_cluster.demo.endpoint
+  token = digitalocean_kubernetes_cluster.demo.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.demo.kube_config[0].cluster_ca_certificate
+  )
+}
+
+provider "null" {}
+
+provider "time" {}
